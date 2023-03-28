21c78ae8a584235dfcfb8329eaabd716176a756b
ZMP <zane.prunella@gmail.com>
2023-03-28 14:37:47 -0700

    vue proj

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,82 @@
+<template>
+  <base-card>
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
+      >Stored Resources</base-button
+    >
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResButtonMode"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <KeepAlive>
+    <component :is="selectedTab"></component>
+  </KeepAlive>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+  components: { StoredResources, AddResource },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official guide',
+          title: ' Official Guide',
+          description: 'The official Vue js doc',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'GOOGLE',
+          title: ' GOOGLE',
+          description: 'The official Google',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+      addResource: this.addRescource,
+      deleteResource: this.removeResource,
+    };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    },
+  },
+
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+    addRescource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: url,
+      };
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'stored-resources';
+    },
+    removeResource(resId) {
+      const resIndex = this.storedResources.findIndex(
+        (res) => res.id === resId
+      );
+      this.storedResources.splice(resIndex, 1);
+    },
+  },
+};
+</script>
